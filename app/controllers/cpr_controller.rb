852778439bf378b9cf4c9ecdbe444d84ea756a28
Steve Work <steve-github@renlabs.com>
2008-08-14 00:13:34 -0700

    Text changes per Erica

--- app/controllers/cpr_controller.rb
+++ app/controllers/cpr_controller.rb
@@ -6,6 +6,7 @@ class CprController < ApplicationController
     sids = @sessions.map {|s| s.id}
 
     @people = Person.find(:all).select {|p| not sids.member? p.session_id}
+    @people = @people.sort {|a,b| a.name <=> b.name}
     people_options = @people.map {|p| "<option value=\"#{p.id}\">#{p.name}</option>"}
     @people_options_string = people_options.join "\n"
   end
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,9 @@ class Person < ActiveRecord::Base
   belongs_to :session
   validates_presence_of :name
 
+  # Why is this not found in cpr_controller.rb?  (Need to reload the server?)
+  def all_not_reserved(session_ids)
+    people = Person.find(:all).select {|p| not session_ids.member? p.session_id}
+    people.sort {|a,b| a.name <=> b.name}
+  end
 end
