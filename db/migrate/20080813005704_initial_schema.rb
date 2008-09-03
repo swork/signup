81ea683eb1cb866382817dc5d1b486d62acf3f3f
Steve Work <steve-github@renlabs.com>
2008-09-02 17:50:00 -0700

    Lose table "operations"

--- db/migrate/20080813005704_initial_schema.rb
+++ db/migrate/20080813005704_initial_schema.rb
@@ -1,14 +1,7 @@
 class InitialSchema < ActiveRecord::Migration
   def self.up
-    create_table :operations do |t|
-      t.string :what
-      t.integer :person_id, :default => nil
-      t.integer :session_id, :default => nil
-      t.timestamps
-    end
   end
 
   def self.down
-    drop_table :operations
   end
 end
